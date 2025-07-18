4df47f6e34102648f488a3703d84948fe48b629d
Daniil <Danielmalik221@gmail.com>
2025-07-18 20:56:59 +0300

    dsds

--- shallow-and-deep.js
+++ shallow-and-deep.js
@@ -1,17 +1,5 @@
-const original =  {
-  name: "John Doe",
-  age: 30,
-  city: "New York",
-  hobbies: [
-    "reading",
-    "cooking",
-    "hiking",
-  ],
-  address: {
-    street: "123 Main Street",
-    zipCode: "10001",
-  },
-}
+
+
 
 const shallowCopy = { ...original};
 
