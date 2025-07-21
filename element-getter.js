9c876bb2986b9a9a9b1896cc239f5bf228af2822
Daniil <Danielmalik221@gmail.com>
2025-07-21 15:48:34 +0300

    ds

--- element-getter.js
+++ element-getter.js
@@ -1,14 +1,19 @@
 
 function getFirstElement(array) {
-  if (Array.isArray(array) && array.length > 0) {
+  if (Array.isArray(array) && array.length >= 0) {
     return [array[0]]
+  }
+  else if (array == "") {
+    return undefined
   }
 }
 
 
 function getLastElement(array) {
-  if (Array.isArray(array) && array.length > 0) {
+  if (Array.isArray(array) && array.length >= 0) {
     return [array[array.length - 1]]
+  } else if (array == "") {
+    return undefined
   }
 }
 
@@ -16,6 +21,8 @@ function getLastElement(array) {
 function getElementByIndex(array, index) {
   if (Array.isArray(array) && index >= 0 && index < array.length) {
     return [array[index]]
-  }
+  } else if (array == "") {
+  return undefined
+}
 }
 
