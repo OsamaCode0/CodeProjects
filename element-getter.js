3b80ac8f8446b6d7ee4cd08bfeee667b1738991d
Daniil <Danielmalik221@gmail.com>
2025-07-21 15:56:12 +0300

    ds

--- element-getter.js
+++ element-getter.js
@@ -1,28 +1,17 @@
 
-function getFirstElement(array) {
-  if (Array.isArray(array) && array.length >= 0) {
-    return [array[0]]
-  }
-  else if (array == "") {
-    return undefined
-  }
+function getFirstElement(input) {
+
+  return input && input.length ? input[0] : undefined;
 }
 
 
-function getLastElement(array) {
-  if (Array.isArray(array) && array.length >= 0) {
-    return [array[array.length - 1]]
-  } else if (array == "") {
-    return undefined
-  }
+function getLastElement (input) {
+  return input && input.length ? input[input.length - 1] : undefined;
 }
 
 
-function getElementByIndex(array, index) {
-  if (Array.isArray(array) && index >= 0 && index < array.length) {
-    return [array[index]]
-  } else if (array == "") {
-  return undefined
+function getElementByIndex(inout, index) {
+  return input && input.length ? input[index] : undefined
 }
-}
+
 
