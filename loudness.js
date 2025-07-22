db420e31e4e7aa3c8715ff1b486f8e1da7f24805
Daniil <Danielmalik221@gmail.com>
2025-07-22 18:54:59 +0300

    ds

---
+++ loudness.js
@@ -0,0 +1,15 @@
+
+
+function makeLouder(string) {
+  string = string.toUpperCase(string);
+
+  return newString;
+}
+
+
+function makeQuieter(string) {
+
+  string = string.toLowerCase(string);
+
+  return string;
+}
