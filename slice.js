7025cbbabda155f18bbe510719efbf434e97f337
Daniil <Danielmalik221@gmail.com>
2025-07-22 22:44:48 +0300

    ds

---
+++ slice.js
@@ -0,0 +1,27 @@
+
+
+
+function extraContent(start, end, content) {
+
+  if (Array.isArray(content)) {
+    let from = start ? content.indexOf(start) : 0;
+    let to = end ? content.indexOf(end) : content.length - 1;
+    return content.slice(from, to + 1);
+  } 
+
+  if (from === -1 || to === -1 || from > to) {
+    return [];
+  }
+
+
+  if (typeof content === 'string'){
+    let from = start ? content.indexOf(start) : 0
+    let to = end ? content.lastOfIndex(end) : content.length - 1;
+  }
+
+  if (from === -1 || to === -1 || from > to) {
+    return '';
+  }
+
+  return content.slice(from, to + 1);
+}
