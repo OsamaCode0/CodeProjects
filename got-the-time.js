08a9cc52ab8fc1c195c2d8d4b9e1fcae23b03e97
Daniil <Danielmalik221@gmail.com>
2025-07-27 19:08:03 +0300

    dsds

---
+++ got-the-time.js
@@ -0,0 +1,16 @@
+
+
+function printPrettyDate(Date) {
+
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+   const day = days[Date.getDay()];
+   const month = months[Date.getMonth()];
+   const date = Date.getDate();
+   const year = Date.getFullYear();
+
+
+  return `${day}, ${month} ${date}, ${year}`;
+}
