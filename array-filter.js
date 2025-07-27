e252e8a5758a0bf3fe56ab2cb858c9495f6c655c
Daniil <Danielmalik221@gmail.com>
2025-07-27 20:59:35 +0300

    ds

---
+++ array-filter.js
@@ -0,0 +1,34 @@
+
+
+
+function filterOutOddNumbers(arrayNumbers) {
+
+
+  const evenNumbers = arrayNumbers.filter(function(arrayNumber){
+    return arrayNumber % 2 === 0;
+  }) 
+
+  return evenNumbers;
+}
+
+function filterObjectsByNameLength(arrayObject, maxLength) {
+ filterObject = arrayObject.filter(function(object) {
+  return object.name.lenght <= maxLength
+ }) 
+
+ return filterObject;
+}
+
+
+function compoundFilter(arrayProduct) {
+
+  const filteredProducts = arrayProduct.filter(function(products) {
+    return (typeof products.code === "string" && products.code.length > 5 && !products.category.includes("special")) && products.price > 50 && products.location !== "Underground";
+  })
+
+  return filteredProducts;
+}
+
+
+
+
