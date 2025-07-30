48b14c46751218ebee8c3896ec20566485d7914c
Daniil <Danielmalik221@gmail.com>
2025-07-30 08:44:41 +0300

    ds

--- diesel.js
+++ diesel.js
@@ -1,55 +1,36 @@
-
-
-
-
-function calculateFines (carsData) {
-
-  //Take stringified 2D array and of car data
-
+function calculateFines(carsData) {
   const cars = JSON.parse(carsData);
   let finedCars = [];
-  let totalFines = 0
+  let totalFines = 0;
 
-  //Initialize an array to hold the fines
-  //Iterate through each car object
-
-if (cars == null || cars.length === 0) {
-      return JSON.stringify({ cars: [], totalFines: 0 });
-    }
+  if (!cars || cars.length === 0) {
+    return JSON.stringify({ totalFines: 0, cars: [] });
+  }
 
   for (let car of cars) {
     const [make, model, reg, year, fuel] = car;
     let fine = 0;
 
     if (year < 2000) fine = 20;
-    else if (fuel == "diesel" && year < 2015) fine = 10;
-
-    
+    else if (fuel === "diesel" && year < 2015) fine = 10;
 
     if (fine > 0) {
       totalFines += fine;
-      finedCars.push
-      ( {  reg, 
-          year,
-           fuel,
-            fine })
+      finedCars.push({ reg, year, fuel, fine });
     }
-    }
-
-
-
-    return JSON.stringify({
-      totalFines,
-      cars: finedCars,
-
-    })
   }
 
+  return JSON.stringify({
+    totalFines,
+    cars: finedCars,
+  });
+}
 
-  function main () {
+function main() {
+  const carData =
+    '[["Toyota", "Camry", "ABC123", 2014, "diesel"], ["Ford", "Focus", "XYZ456", 1999, "petrol"]]';
 
-    const carData = '[["Toyota", "Camry", "ABC123", 2014, "diesel"], ["Ford", "Focus", "XYZ456", 1999, "petrol"]]'
+  console.log(calculateFines(carData));
+}
 
-console.log(calculateFines(carData))
-  }
-  main();
+main();
