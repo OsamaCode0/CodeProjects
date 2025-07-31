86c7eb73647a4416fd55c3c33d491fdfd9ad45be
Daniil <Danielmalik221@gmail.com>
2025-08-01 02:34:51 +0300

    ds

--- get-el.js
+++ get-el.js
@@ -3,26 +3,31 @@
 
 function getElementsByTag(tagName) {
 
-  gottenElement = document.getElementsByTagName(tagName)
+  let gottenElement = document.getElementsByTagName(tagName)
 
   return gottenElement
 }
 
 function getElementsByClassName(className) {
 
-  gottenClass = document.getElementsByClassName(className)
+  let gottenClass = document.getElementsByClassName(className)
   return gottenClass
 }
 
 function getElementById(tagId) {
 
-  gottenId = document.getElementById(tagId) 
+  let gottenId = document.getElementById(tagId) 
+
+  if (gottenId == null) {
+    return undefined
+  }
+
   return gottenId
 }
 
 
 function getElementsByAttribute(tagAttribute) {
 
-  gottenAttribute = document.getElementsByAttribute(tagAttribute)
+ let gottenAttribute = document.getElementsByAttribute(tagAttribute)
   return gottenAttribute
 }
