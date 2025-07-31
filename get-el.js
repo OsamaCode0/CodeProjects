3cbab2d0c3ced89d3de5201722affc32c4980af0
Daniil <Danielmalik221@gmail.com>
2025-08-01 02:32:45 +0300

    ds

---
+++ get-el.js
@@ -0,0 +1,28 @@
+
+
+
+function getElementsByTag(tagName) {
+
+  gottenElement = document.getElementsByTagName(tagName)
+
+  return gottenElement
+}
+
+function getElementsByClassName(className) {
+
+  gottenClass = document.getElementsByClassName(className)
+  return gottenClass
+}
+
+function getElementById(tagId) {
+
+  gottenId = document.getElementById(tagId) 
+  return gottenId
+}
+
+
+function getElementsByAttribute(tagAttribute) {
+
+  gottenAttribute = document.getElementsByAttribute(tagAttribute)
+  return gottenAttribute
+}
