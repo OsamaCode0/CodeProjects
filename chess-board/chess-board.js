b659994a335938bbba74e2a6be8bf6eb42125d61
Daniil <Danielmalik221@gmail.com>
2025-08-03 01:27:40 +0300

    ds

--- chess-board/chess-board.js
+++ chess-board/chess-board.js
@@ -1,28 +1,25 @@
 function initializeChessboard() {
-  const chessboard = document.createElement('div');
-  chessboard.classList.add('chessboard');
-  document.body.appendChild(chessboard);
-
+  const board = document.getElementById("chessboard");
   let selectedSquare = null;
 
   for (let row = 1; row <= 8; row++) {
-    for (let col = 1; col <= 8; col++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      square.id = `square-${row}-${col}`;
+    for (let column = 1; column <= 8; column++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.id = `r${row}-c${column}`;
 
-      const isWhite = (row + col) % 2 === 0;
-      square.classList.add(isWhite ? 'white' : 'black');
+      const isWhite = (row + column) % 2 === 0;
+      square.classList.add(isWhite ? "white" : "black");
 
-      square.addEventListener('click', () => {
+      square.addEventListener("click", () => {
         if (selectedSquare) {
-          selectedSquare.classList.remove('selected');
+          selectedSquare.classList.remove("selected");
         }
-        square.classList.add('selected');
+        square.classList.add("selected");
         selectedSquare = square;
       });
 
-      chessboard.appendChild(square);
+      board.appendChild(square);
     }
   }
 }
