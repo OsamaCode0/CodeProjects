8bc61b7484d94d3bf9a24ef564fa0046e1410587
Daniil <Danielmalik221@gmail.com>
2025-09-16 23:35:35 +0300

    ds

---
+++ sensor-readings.ts
@@ -0,0 +1,13 @@
+
+
+let sensorReading1: {type: string, value: number, isCritical? : boolean} = {
+  type: 'temprature',
+  value: 25,
+  isCritical: false
+}
+
+let sensorReading2: {type: string, value: number, isCritical? : boolean} = {
+
+  type: 'radiotion',
+  value: 150,
+}
