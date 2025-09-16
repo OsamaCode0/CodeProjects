2027857f69c9e83eff285fcae6b7761e43b52877
Daniil <Danielmalik221@gmail.com>
2025-09-17 02:07:12 +0300

    ds

--- shield-status.ts
+++ shield-status.ts
@@ -1,7 +1,5 @@
 
 
-type shieldStatus = string | number;
-
-let mainShieldStatus: shieldStatus = 68;
-
-let subShieldStatus: shieldStatus = "BAD";
+type ShieldStatus = string | number;
+let mainShieldHealth: ShieldStatus = 68;
+let subShieldState: ShieldStatus = "BAD";
