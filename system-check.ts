a2c6d0e2f9948281ad28258ad640f18ea3fdd9fe
Daniil <Danielmalik221@gmail.com>
2025-09-18 01:32:05 +0300

    ds

---
+++ system-check.ts
@@ -0,0 +1,39 @@
+
+
+type Component =
+| string
+| number
+| {type: 'sensor'; status: string}
+| {type: 'motor'; speed: string}
+
+function getComponentInfo(component: Component): string {
+
+  if (typeof component === 'string') {
+      return `Component ID: ${component}`;
+  }
+
+  if (typeof component === 'number') {
+      return `Component Value: ${component}`;
+  }
+
+  if (typeof component === 'object') {
+    if ('object' === null) {
+        return "Unknown Object Component";
+    }
+  }
+  if ('type' in component) {
+    if (component.type === 'sensor') {
+        return `Sensor Status: ${component.status}`;
+    } else if (component.type === 'motor') {
+        return `Motor Speed: ${component.speed}`;
+
+    }
+  }
+
+  // If the object does not match known types, return unknown
+  return "Unknown Object Component";
+  
+  if (typeof component !== 'string' && typeof component !== 'number') {
+      return "Invalid Component Type";
+  }
+}
