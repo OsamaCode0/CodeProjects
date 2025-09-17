c1f92d065be6fc3382845f1a94c6cc00f7683e7f
Daniil <Danielmalik221@gmail.com>
2025-09-18 00:34:45 +0300

    ds

--- telemetry-callback.ts
+++ telemetry-callback.ts
@@ -3,6 +3,6 @@
 
 type TelemetryCallback = (sensorReading: number, time: number) => void;
 
-function logTelematry(data: number, timestamp: number): void {
+function logTelemetry(data: number, timestamp: number): void {
     console.log(`Data: ${data}, Timestamp: ${timestamp}`)
 }
