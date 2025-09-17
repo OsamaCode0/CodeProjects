8d3a33af8783ebc42ef79ae29aad59bc5a0a4a56
Daniil <Danielmalik221@gmail.com>
2025-09-17 08:39:21 +0300

    ds

--- thruster-control.ts
+++ thruster-control.ts
@@ -3,21 +3,19 @@ type Thruster = {name: string; powerLevel: number}
 
 type Thrusters = {left: Thruster; right: Thruster; main: Thruster}
 
-let thursters: Thrusters = {
+let thrusters: Thrusters = {
      left: {name: 'left', powerLevel: 0},
      right: {name: 'right', powerLevel: 0},
      main: {name: 'main', powerLevel: 0}, 
 }
 
-function setThrustPower(thurster: string, powerLevel: number): void {
-  if (powerLevel < 0 || powerLevel > 100) {
+function setThrusterPower(thruster: string, powerLevel: number): void {
+  if (powerLevel < 0 && powerLevel > 100) {
     throw new Error('Power level must be between 0 and 100');
   }
-  if (thurster in thursters) {
-    thursters[thurster as keyof Thrusters].powerLevel = powerLevel;
-  } else {
-    throw new Error('Invalid thruster name');
+  if (thruster in thrusters) {
+    thrusters[thruster as keyof Thrusters].powerLevel = powerLevel;
   }
-}  {
 
 }
+
