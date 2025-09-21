1b9c2a7b3de55531de018b60c99e3d4e573d2537
Daniil <Danielmalik221@gmail.com>
2025-09-21 18:21:57 +0300

    ds

---
+++ fuel-input.ts
@@ -0,0 +1,23 @@
+
+
+let form = document.getElementById('fuelForm') as HTMLFormElement;
+
+let fuelInput = document.getElementById('submitFuel') as HTMLInputElement;
+
+let message = document.getElementById('message') as HTMLParagraphElement;
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  let valueFuel = fuelInput.value.trim();
+  let fuelNumber = Number(valueFuel);
+
+  if (valueFuel === '' || isNaN(fuelNumber)) {
+    message.textContent = 'Please enter a valid number for fuel.';
+  }
+
+  if (fuelNumber < 0 || fuelNumber > 100) {
+    message.textContent = "Invalid fuel input. Please enter a value between 0 and 100.";
+  } else {
+    message.textContent = "Fuel level set to:" [fuelNumber]
+  }
+})
